6866518708747a29c9c5017fd61e5c5c5f710dd7
Justin Saunders <jls2245@columbia.edu>
2017-04-19 15:12:09 -0400

    smashing my head against JS

--- Final Project/js/project.js
+++ Final Project/js/project.js
@@ -1,3 +1,33 @@
-var userInput = ""
+var withGrade = [];
+var sortedByDate = [];
 
-$.getJSON("https://data.cityofnewyork.us/resource/43nn-pn8j.json?dba="
+$("button").on("click", function() {
+  var userInput = $("input").val();
+  var userBoro = $("#boro").val();
+
+  $.getJSON("https://data.cityofnewyork.us/resource/43nn-pn8j.json?$q=" + userInput + "&boro=" + userBoro, function(data) {
+      var dataIndex = 0;
+
+      var gradeAvailable = ["grade"] in data[dataIndex];
+
+      var gradedRestaurant = {
+          grade: data[dataIndex]["grade"],
+          violation: data[dataIndex]["violation_description"],
+        }
+          
+      while (dataIndex < data.length) {
+
+        _.filter(data, gradeAvailable = true);
+
+        withGrade.push(data);
+ 
+
+        dataIndex = dataIndex + 1;
+      }
+  });
+});
+
+// d = "2016-05-31T00:00:00"
+// "2016-05-31T00:00:00"
+// new Date(d)
+// Mon May 30 2016 20:00:00 GMT-0400 (Eastern Daylight Time)
